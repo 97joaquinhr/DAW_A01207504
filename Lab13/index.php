7587d232c3bc44fa69bb2ab86e5213f10d012b9a
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-02-25 14:41:33 -0600

    worked on index.php and login.php

--- Lab13/index.php
+++ Lab13/index.php
@@ -1,3 +1,11 @@
-<?php include("../Lab9/partials/_header.html"); 
-include("Partials/_login.html");
-include("../Lab9/partials/_footer.html"); ?>
+<?php 
+    session_start();
+    if(isset($_SESSION["usrname"])){
+        header("location: login.php");
+    }
+    else{
+        include("../Lab9/partials/_header.html"); 
+        include("Partials/_login.html");
+        include("../Lab9/partials/_footer.html");
+    }
+ ?>
