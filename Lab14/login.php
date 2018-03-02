d7089d855da6cb4fe6f2cfab595b3081bae11f13
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-03-01 20:49:12 -0600

    init commit lab14

---
+++ Lab14/login.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    include("../Lab9/partials/_header.html");
+    if($_SESSION["usrname"]=="usuario"){
+        include("Partials/_Success.html");
+    }
+    else if(htmlspecialchars($_POST["usrname"])=="usuario" && htmlspecialchars($_POST["psw"])=="Password1!"){
+        unset($_SESSION["error"]);
+        $_SESSION["usrname"]=$_POST["usrname"];
+        $_SESSION["producto"]=" ";
+        include("Partials/_Success.html");
+    }
+    else{
+        $_SESSION["error"] = "Usuario y/o contraseña incorrectos";
+        header("location: index.php");
+    }
+    include("../Lab9/partials/_footer.html");
+?>
