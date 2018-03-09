ec399b59cc87ee3ddc2999dd781437d33d20d31c
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-03-09 14:02:41 -0600

    init commit Lab 19, preguntas listas

---
+++ Lab19/login.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require_once("util.php");
+    include("../Lab9/partials/_header.html");
+    if(isset($_SESSION["usrname"])){
+        if($_SESSION["usrname"]=="usuario"){
+            include("Partials/_Success.html");
+        }
+    }
+    else if(login(htmlspecialchars($_POST["usrname"]), htmlspecialchars($_POST["psw"]))){
+        unset($_SESSION["error"]);
+        $_SESSION["usrname"]=$_POST["usrname"];
+        $_SESSION["producto"]=" ";
+        include("Partials/_Success.html");
+    }
+    else{
+        $_SESSION["error"] = "Usuario y/o contraseña incorrectos";
+        header("location: index.php");
+    }
+    include("../Lab9/partials/_footer.html");
+?>
