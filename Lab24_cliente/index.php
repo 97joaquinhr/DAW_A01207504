80c210fb2726f0e2853fe88abf4bc3ee02ee8b7c
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-04-12 18:50:49 -0500

    agrega a base de datos un soruce de slim

--- Lab24_cliente/index.php
+++ Lab24_cliente/index.php
@@ -1,8 +1,28 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: joaqu
+ * Date: 4/12/2018
+ * Time: 5:59 PM
+ */
+session_start();
+require_once("util.php");
+if(isset($_SESSION["usrname"])){
+    header("location: login.php");
+}
+else{
+    include("../Lab9/partials/_header.html");
+//    include("Partials/_login.html")
     $name = "lalo";
     $url = "http://localhost:8080/DAW_A01207504/Lab24_servicio/public/$name"; //Route to the REST web service
     $c = curl_init($url);
     $response = curl_exec($c);
     curl_close($c);
-    //var_dump($response); 
-?>
+    $url = "http://localhost:8080/DAW_A01207504/Lab24_servicio/public/bd/insertar"; //Route to the REST web service
+    $c = curl_init($url);
+    $response = curl_exec($c);
+    curl_close($c);
+    echo "<p>Cuando entra a la página, inserta una entrada en la base de datos con CrearProducto() del util.php</p>";
+//var_dump($response);
+    include("../Lab9/partials/_footer.html");
+}
---
+++ Lab24_cliente/util.php
@@ -0,0 +1,177 @@
+<?php
+    function connect() {
+        $mysql = mysqli_connect("localhost","root","","lab14");
+                                            //root si estan en windows
+        $mysql->set_charset("utf8");
+        return $mysql;
+    }
+    
+    function disconnect($mysql) {
+        mysqli_close($mysql);
+    }
+function crearProducto($nombre, $imagen) {
+    $db = connect();
+
+    if ($db != NULL) {
+
+        // insert command specification
+        $query='INSERT INTO productos (nombre,imagen) VALUES (?,?)';
+        // Preparing the statement
+        if (!($statement = $db->prepare($query))) {
+            die("Preparation failed: (" . $db->errno . ") " . $db->error);
+        }
+        // Binding statement params
+        if (!$statement->bind_param("ss", $nombre, $imagen)) {
+            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error);
+        }
+        // Executing the statement
+        if (!$statement->execute()) {
+            die("Execution failed: (" . $statement->errno . ") " . $statement->error);
+        }
+
+
+        disconnect($db);
+        return true;
+    }
+    return false;
+}
+    function login($user, $passwd) {
+        $db = connect();
+        if ($db != NULL) {
+            
+            //Specification of the SQL query
+            $query='SELECT nombre FROM usuarios WHERE nombre="'.$user.
+                '" AND passwd="'.$passwd.'"';
+            $query;
+             // Query execution; returns identifier of the result group
+            $results = $db->query($query);
+             // cycle to explode every line of the results
+             
+            if (mysqli_num_rows($results) > 0)  {
+                // it releases the associated results
+                mysqli_free_result($results);
+                disconnect($db);
+                return true;
+            }
+            return false;
+        } 
+        return false;
+    }
+    function getTable($tabla) {
+        $db = connect();
+        if ($db != NULL) {
+            
+            //Specification of the SQL query
+            $query='SELECT * FROM '.$tabla;
+            $query;
+             // Query execution; returns identifier of the result group
+            $results = $db->query($query);
+             // cycle to explode every line of the results
+        
+           $html = '<table class="striped">';
+           $html .= '<thead>';
+           $html .= '<tr>';
+           $columnas = $results->fetch_fields();
+           for($i=0; $i<count($columnas); $i++) {
+                $html .= '<th>'.$columnas[$i]->name.'</th>';
+           }
+           $html .= '</tr>';
+           $html .= '</thead>';
+           
+           $html .= '<tbody>';
+           while ($fila = mysqli_fetch_array($results, MYSQLI_BOTH)) {
+                                                // Options: MYSQLI_NUM to use only numeric indexes
+                                                // MYSQLI_ASSOC to use only name (string) indexes
+                                                // MYSQLI_BOTH, to use both
+                    $html .= '<tr>';
+                    for($i=0; $i<count($fila); $i++) {
+                        // use of numeric index
+                        $html .= '<td>'.$fila[$i].'</td>'; 
+                    }
+                    $html .= '</tr>';
+            }
+            $html .= '</tbody></table>';   
+            // it releases the associated results
+            mysqli_free_result($results);
+            disconnect($db);
+            return $html;
+        }
+        return false;
+    }
+    
+    function getProductosRaw() {
+        $db = connect();
+        if ($db != NULL) {
+            
+            //Specification of the SQL query
+            $query='SELECT * FROM productos';
+            $query;
+             // Query execution; returns identifier of the result group
+            $results = $db->query($query);
+             // cycle to explode every line of the results
+        
+           while ($fila = mysqli_fetch_array($results, MYSQLI_NUM)) {
+                                                // Options: MYSQLI_NUM to use only numeric indexes
+                                                // MYSQLI_ASSOC to use only name (string) indexes
+                                                // MYSQLI_BOTH, to use both
+                    for($i=0; $i<count($fila); $i++) {
+                        // use of numeric index
+                        echo $fila[$i].' '; 
+                    }
+                    echo '<br>';
+            }
+               
+            // it releases the associated results
+            mysqli_free_result($results);
+            disconnect($db);
+            return true;
+        }
+        return true;
+    }
+    
+    function getProductos() {
+        $db = connect();
+        if ($db != NULL) {
+            
+            //Specification of the SQL query
+            $query='SELECT * FROM productos';
+            $query;
+             // Query execution; returns identifier of the result group
+            $results = $db->query($query);
+             // cycle to explode every line of the results
+            $html = '<div class="grid-container"> 
+                        <div class="grid-x grid-padding-x small-up-2 medium-up-3">';
+           while ($fila = mysqli_fetch_array($results, MYSQLI_BOTH)) {
+                                                // Options: MYSQLI_NUM to use only numeric indexes
+                                                // MYSQLI_ASSOC to use only name (string) indexes
+                                                // MYSQLI_BOTH, to use both
+                    $html .= '<div class="cell">
+                                <div class="card">
+                                    <div class="card-divider">
+                                        '.$fila["nombre"].'
+                                    </div>
+                                    <img src="uploads/'.$fila["imagen"].'">
+                                    <div class="card-section">
+                                      <p>Publicado el: '.$fila["created_at"].'.</p>
+                                      <a href="Veditar.php" class="button" role="button">Editar</a>
+                                    </div>
+                                </div>
+                            </div>';
+            }
+            $html .='</div>
+                </div>';
+            
+            echo $html;
+            // it releases the associated results
+            mysqli_free_result($results);
+            disconnect($db);
+            return true;
+        }
+        return true;
+    }
+        
+    
+    //var_dump(login('lalo', 'hockey'));
+    //var_dump(login('joaquin', 'basket'));
+    //var_dump(login('cesar', 'basket'));
+?>
